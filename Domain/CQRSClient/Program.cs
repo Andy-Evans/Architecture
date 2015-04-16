4ccd110b1430eecdddfe0fb87da139dd8d214dce
unknown <andrew.evans@SSLWSWIN7002.Airsweb.local>
2015-04-16 13:54:40 +0100

    Added CQRS Code

---
+++ Domain/CQRSClient/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace CQRSClient
+{
+    class Program
+    {
+        private static MessageBus bus;
+
+        static void Main(string[] args)
+        {
+            bus = new MessageBus();
+
+            var commands = new CustomerCommandHandlers(/*inject repo here*/);
+            bus.RegisterHandler<AddCustomer>(commands.Handle);
+            bus.RegisterHandler<UpdateCustomerName>(commands.Handle);
+            bus.RegisterHandler<UpdateCustomerMarketingPreferences>(commands.Handle);
+            bus.RegisterHandler<DeleteCustomer>(commands.Handle);
+
+            AddCustomer();
+
+            UpdateName();
+
+            UpdateMarketingPreferences();
+
+            DeleteCustomer();
+        }
+
+        private static void AddCustomer()
+        {
+            var command = new AddCustomer(new Customer());
+            bus.Send(command);
+        }
+
+        private static void UpdateName()
+        {
+            var command = new UpdateCustomerName(new CustomerName());
+            bus.Send(command);
+        }
+
+        private static void UpdateMarketingPreferences()
+        {
+            var command = new UpdateCustomerMarketingPreferences(new CustomerMarketingPreferences());
+            bus.Send(command);
+        }
+
+        private static void DeleteCustomer()
+        {
+            var command = new DeleteCustomer(1);
+            bus.Send(command);
+        }
+    }
+}
---
+++ Domain/MessageBus.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Domain
+{
+    public class MessageBus : ICommandSender
+    {
+        private readonly Dictionary<Type, List<Action<Message>>> _routes = new Dictionary<Type, List<Action<Message>>>();
+
+        public void RegisterHandler<T>(Action<T> handler) where T : Message
+        {
+            List<Action<Message>> handlers;
+
+            if (!_routes.TryGetValue(typeof(T), out handlers))
+            {
+                handlers = new List<Action<Message>>();
+                _routes.Add(typeof(T), handlers);
+            }
+
+            handlers.Add((x => handler((T)x)));
+        }
+
+        public void Send<T>(T command) where T : Command
+        {
+            List<Action<Message>> handlers;
+
+            if (_routes.TryGetValue(typeof(T), out handlers))
+            {
+                if (handlers.Count != 1) throw new InvalidOperationException("cannot send to more than one handler");
+                handlers[0](command);
+            }
+            else
+            {
+                throw new InvalidOperationException("no handler registered");
+            }
+        }
+    }
+
+    public interface ICommandSender
+    {
+        void Send<T>(T command) where T : Command;
+
+    }
+}
